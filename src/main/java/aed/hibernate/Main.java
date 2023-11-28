24c334e12d41b6ba60ed68758c526e388457cc24
Miguel Ángel Lombana García <84014018+miguellombana@users.noreply.github.com>
2023-11-28 12:53:39 +0000

    V0.0.4

--- src/main/java/aed/hibernate/Main.java
+++ src/main/java/aed/hibernate/Main.java
@@ -6,93 +6,111 @@ public class Main {
 
 	public static void main(String[] args) {
 
-			Session sesion = HibernateUtil.getSessionFactory().openSession(); // crea la sesion
-			sesion.beginTransaction();
+		Session sesion = HibernateUtil.getSessionFactory().openSession(); // crea la sesion
+		sesion.beginTransaction();
+
+		try {
+
+			addFamilia("Cereales", sesion);
+			addFamilia("Bebidas", sesion);
+			addFamilia("Congelados", sesion);
+			addFamilia("Consumibles", sesion);
+
+			Familia verduras = new Familia();
+			verduras.setDenoFamilia("Verduras");
+			sesion.persist(verduras);
 			
-			try {
-				
-				addFamilia("Cereales",sesion);
-				addFamilia("Bebidas",sesion);
-				addFamilia("Congelados",sesion);
-				addFamilia("Consumibles",sesion);
-				
-	            Familia familia = new Familia();
-	            familia.setDenoFamilia("Verduras");
-	            sesion.persist(familia);
-	            
-	            
-	            addProducto("Producto1", 10.00, familia, false, sesion);
-				
-				
-	          
-/*
- * 
- * 
+			Familia frutas = new Familia();
+			frutas.setDenoFamilia("Frutas");
+			sesion.persist(frutas);
 
-	            
-	            Producto producto = new Producto();
-	            producto.setCodProducto(1);
-	            producto.setDenoProducto("Manzanas");
-	            producto.setcodFamilia(familia);
-	            producto.setCongelado(false);
-	           sesion.persist(producto);
-	        
+			Familia congelados = new Familia();
+			congelados.setDenoFamilia("Congelados");
+			sesion.persist(congelados);
+			
+			Tienda tienda1 = new Tienda();
+			tienda1.setDenoTienda("T001");
+			tienda1.setCodigoPostal("38370");
+			sesion.persist(tienda1);
 
-	            Tienda tienda = new Tienda();
-	            tienda.setCodTienda(1);
-	            tienda.setDenoTienda("Mercadona");
-	            tienda.setCodigoPostal("10001");
-	           sesion.persist(tienda);
-	       
+			
+			
 
-	            Stock stock = new Stock();
-	            stock.setCodProducto(1);
-	            stock.setCodTienda(1);
-	            stock.setUnidades(100);
-	           sesion.persist(stock);
-	        
+			Producto a = addProducto("Lechugas", 10.00, verduras, false, sesion);
+			addProducto("Tomates", 10.00, verduras, false, sesion);
+			addProducto("Manzanas", 10.00, frutas, false, sesion);
+			addProducto("Gambas", 10.00, congelados, true, sesion);
+			
+			
+			Stock stock1 = new Stock();
+			stock1.setCodProducto(a);
+			stock1.setCodTienda(tienda1);
+			stock1.setUnidades(100);
+			sesion.persist(stock1);
+			
+			
 
-	            ProductoObservacion productoObservacion = new ProductoObservacion();
-	            //productoObservacion.setCodProducto(1);
-	            productoObservacion.setObservacion("Todo correctoiiii");
-	            sesion.persist(productoObservacion);*/
-	            sesion.getTransaction().commit();
-	        } catch (Exception e) {
-	            // En caso de error, realiza un rollback
-	            sesion.getTransaction().rollback();
-	            e.printStackTrace(); // Manejo de excepciones adecuado
-	        } finally {
-	            // Cierre de la sesión al final
-	            sesion.close();
-	        }
-	    }
-	
-	
+			/*
+			 * 
+			 * 
+			 * 
+			 * 
+			 * Producto producto = new Producto(); producto.setCodProducto(1);
+			 * producto.setDenoProducto("Manzanas"); producto.setcodFamilia(familia);
+			 * producto.setCongelado(false); sesion.persist(producto);
+			 * 
+			 * 
+			 * Tienda tienda = new Tienda(); tienda.setCodTienda(1);
+			 * tienda.setDenoTienda("Mercadona"); tienda.setCodigoPostal("10001");
+			 * sesion.persist(tienda);
+			 * 
+			 * 
+			 * Stock stock = new Stock(); stock.setCodProducto(1); stock.setCodTienda(1);
+			 * stock.setUnidades(100); sesion.persist(stock);
+			 * 
+			 * 
+			 * ProductoObservacion productoObservacion = new ProductoObservacion();
+			 * //productoObservacion.setCodProducto(1);
+			 * productoObservacion.setObservacion("Todo correctoiiii");
+			 * sesion.persist(productoObservacion);
+			 */
+			sesion.getTransaction().commit();
+		} catch (Exception e) {
+			// En caso de error, realiza un rollback
+			sesion.getTransaction().rollback();
+			e.printStackTrace(); // Manejo de excepciones adecuado
+		} finally {
+			// Cierre de la sesión al final
+			sesion.close();
+		}
+	}
+
 	public static void addFamilia(String nombre, Session sesion) {
-		
-        Familia familia = new Familia();
-        familia.setDenoFamilia(nombre);
-        sesion.persist(familia);
-	
+
+		Familia familia = new Familia();
+		familia.setDenoFamilia(nombre);
+		sesion.persist(familia);
+
 	}
-	
-	public static void addProducto(String nombre, Double precio, Familia familia, boolean congelado,   Session sesion) {
-		
-		  Producto 	producto = new Producto();
-		  producto.setDenoProducto(nombre);
-		  producto.setPrecioBase(precio);
-		  producto.setcodFamilia(familia);
-		  producto.setCongelado(false);
-		  
-		  sesion.persist(producto);
-	
+	public static void add(String nombre, Session sesion) {
+
+		Familia familia = new Familia();
+		familia.setDenoFamilia(nombre);
+		sesion.persist(familia);
+
 	}
-	
-	
-	
-	
-	
-	
-	
-	
+
+	public static Producto addProducto(String nombre, Double precio, Familia familia, boolean congelado, Session sesion) {
+
+		Producto producto = new Producto();
+		producto.setDenoProducto(nombre);
+		producto.setPrecioBase(precio);
+		producto.setcodFamilia(familia);
+		producto.setCongelado(congelado);
+
+		sesion.persist(producto);
+		return producto;
+
+	}
+
 }
